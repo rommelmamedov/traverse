e9b22e78eda96630f31c07bd90c4e5bf3f087b8b
franciscawebisoft <francisca@webisoft.com>
2023-01-25 11:46:39 -0500

    TRAV-9 Footer solcial link fix

--- src/components/common/FooterSocialLinks.js
+++ src/components/common/FooterSocialLinks.js
@@ -1,13 +1,16 @@
 import React  from 'react';
 import clsx from "clsx";
-import {BsMedium} from "react-icons/bs";
-import {FaDiscord, FaTelegramPlane, FaTwitter} from "react-icons/fa";
+import { MdOutlineAlternateEmail } from "react-icons/md";
+//import {BsMedium} from "react-icons/bs";
+import {FaTwitter} from "react-icons/fa";
+//import {FaDiscord, FaTelegramPlane} from "react-icons/fa";
 
 const footerSocialLinks = [
-  {name: "Medium", url: "", icon:<BsMedium/>},
-  {name: "Telegram", url: "", icon: <FaTelegramPlane/>},
-  {name: "Discord", url: "", icon: <FaDiscord/>},
-  {name: "Twitter", url: "", icon: <FaTwitter/>}
+  // {name: "Medium", url: "", icon:<BsMedium/>},
+  // {name: "Telegram", url: "", icon: <FaTelegramPlane/>},
+  // {name: "Discord", url: "", icon: <FaDiscord/>},
+  {name: "Twitter", url: "", icon: <FaTwitter/>},
+  {name: "Email", url: "mailto:contact@traverselabs.xyz", icon: <MdOutlineAlternateEmail/>},
 ]
 
 function FooterSocialLinks({extraClass}) {
